cdbbc633e51487d103eb4208a4d1be552f60f127
Pablo-Luiz-Vieira <pablo_lek-13@hotmail.com>
2021-09-20 18:47:50 -0300

    compra de pizzas JS

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:[20.19, 30.00, 35.00], sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:[18.00, 28.00, 38.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:[17.45, 27.45, 37.45], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:[19.77, 29.77, 39.77], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:[21.43, 31.43, 41.43], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:[18.55, 28.55, 38.55], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:[22.36, 32.36, 32.36], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
